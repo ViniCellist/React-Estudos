5b80e3b807cf689c65d847fdf4325367105ac888
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-05-30 13:59:57 -0300

    startpack

---
+++ projeto1/src/App.jsx
@@ -0,0 +1,14 @@
+import Titulo from "./components/Titulo/Titulo"
+
+function App() {
+  return (
+    <>
+      <Titulo cor="red" />
+      <Titulo cor="blue" />
+      <Titulo cor="orange" />
+      <Titulo cor="green" />
+    </>
+  )
+}
+
+export default App
